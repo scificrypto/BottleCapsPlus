6d75d79d83d3f456f53b4129d7436678ea7f1dad
scificrypto <realgrandnagus@gmail.com>
2019-01-28 12:41:20 -0500

    Fix NTP filter

--- src/ntp.cpp
+++ src/ntp.cpp
@@ -463,7 +463,7 @@ void ThreadNtpSamples(void* parg) {
     // Make this thread recognisable as time synchronization thread
     RenameThread("bottlecapsplus-ntp-samples");
 
-    CMedianFilter<int64_t> vTimeOffsets(200,0);
+    CMedianFilter<int64_t> vTimeOffsets(9,0);
 
     while (!fShutdown) {
         if (strTrustedUpstream != "localhost") {
